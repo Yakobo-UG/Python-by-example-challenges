f9187f9441b56c1a50c39e7427602610af6a5b69
Muhumuza James <65670517+Yakobo-UG@users.noreply.github.com>
2021-09-03 18:32:49 +0300

    challenge 21
    Ask the user to enter their first name and then ask them to enter their surname. Join them together with a space between and display the name and the length of whole name.

---
+++ challenge 21.py
@@ -0,0 +1,6 @@
+#Ask the user to enter their first name and then ask them to enter their surname. Join them together with a space between and display the name and the length of whole name. 
+
+First_name = str(input("Enter your first name: "))
+Second_name = str(input("Enter your Second name: "))
+
+print(First_name + " " + Second_name)
