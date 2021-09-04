44c2587e046b3e9efc493c3923325b2b5e1797b3
Muhumuza James <65670517+Yakobo-UG@users.noreply.github.com>
2021-09-04 17:23:06 +0300

    challenge 27
    Ask the user to enter a number with lots of decimal places. Multiply this number by two and display the answer.

---
+++ challenge 27.py
@@ -0,0 +1,5 @@
+#Ask the user to enter a number with lots of decimal places. Multiply this number by two and display the answer.
+
+Decimal = float(input("Enter a decimal number: "))
+multy = Decimal*2
+print(multy)
