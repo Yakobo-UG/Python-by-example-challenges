77b75f448d2075715c2ffd3e2ed97b76c2350c6e
Muhumuza James <65670517+Yakobo-UG@users.noreply.github.com>
2021-09-08 23:46:58 +0300

    challenge 45
    et the total to 0 to start with. While the total is 50 or less, ask the user to input a number. Add that number to the total and print the message “The total is... [total]”. Stop the loop when the total is over 50.

---
+++ challenge 45.py
@@ -0,0 +1,8 @@
+#Set the total to 0 to start with. While the total is 50 or less, ask the user to input a number. Add that number to the total and print the message “The total is... [total]”. Stop the loop when the total is over 50. 
+
+total = 0
+while total <= 50:
+    num = float(input("Enter number: "))
+    total = total + num
+    print("The total is ", total)
+    
