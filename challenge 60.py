b2ff1446d1aae807dfbf97322a82293a062af67a
Muhumuza James <65670517+Yakobo-UG@users.noreply.github.com>
2021-09-17 00:20:36 +0300

    challenge 60
    Draw a square

---
+++ challenge 60.py
@@ -0,0 +1,10 @@
+#Draw a square
+import turtle
+scr = turtle.Screen()
+scr.bgcolor("Pink")
+turtle.color("Blue", "Yellow")
+for i in range(0,4):
+    turtle.forward(100)
+    turtle.right(90)
+
+turtle.exitonclick()
