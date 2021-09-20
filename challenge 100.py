705235e597ae421b23196b99d1b6c23988310662
Muhumuza James <65670517+Yakobo-UG@users.noreply.github.com>
2021-09-21 01:32:59 +0300

    challenge 100
    Create the following using a 2D dictionary showing the sales each person has made in the different geographical regions:

---
+++ challenge 100.py
@@ -0,0 +1,11 @@
+#Create the following using a 2D dictionary showing the sales each person has made in the different geographical regions: 
+'''
+        N   S   E   W
+John    23  65  87  56
+Tom     87  22  77  99
+Peter   73  00  83  27
+Fiona   11  73  27  89
+
+'''
+
+Dic2D = {"N": {"John":  23,  "Tom": 87 , "Peter": 73 , "Fiona": 11 },"S": { "John":65, "Tom":22, "Peter":00, "Fiona":73}, "E": { "John": 87, "Tom": 77, "Peter":83, "Fiona":27},"W": { "John": 56, "Tom": 99, "Peter":27, "Fiona":89}}
