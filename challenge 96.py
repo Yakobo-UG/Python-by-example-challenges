998dd662c6d774b6b3331426445e9e09fe15f09b
Muhumuza James <65670517+Yakobo-UG@users.noreply.github.com>
2021-09-21 01:31:34 +0300

    challenge 96
    Create the following using a simple 2D list using the standard Python indexing:

---
+++ challenge 96.py
@@ -0,0 +1,11 @@
+#Create the following using a simple 2D list using the standard Python indexing:
+'''
+    0   1   2
+0   2   5   8
+1   3   7   4
+2   1   6   9
+3   4   2   0
+
+'''
+Simple2D = [ [2,5,8],[3,7,4], [1,6,9],[34,2,0] ]
+print(Simple2D)
