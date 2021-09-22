e9d1360418bc140ddd70ab50e6288f55e9fb354a
Muhumuza James <65670517+Yakobo-UG@users.noreply.github.com>
2021-09-22 12:50:07 +0300

    challenge 7
    Define a function that can convert a integer into a string and print it in console.
    #Hints:
    #Use str() to convert a number to string.

---
+++ Additional challenges/challenge 7.py
@@ -0,0 +1,8 @@
+#Define a function that can convert a integer into a string and print it in console.
+#Hints:
+#Use str() to convert a number to string.
+def tostring(Num):
+    return str(Num)
+print(tostring(5))
+print(type(tostring(5)))
+
