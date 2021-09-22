1ef499dfbf9a842af4db32548ca51b292fa2b434
Muhumuza James <65670517+Yakobo-UG@users.noreply.github.com>
2021-09-22 12:49:51 +0300

    challenge 6
    Define a function which can compute the sum of two numbers.
    #Hints:
    #Define a function with two numbers as arguments. You can compute the sum in the function and return the value.

---
+++ Additional challenges/challenge 6.py
@@ -0,0 +1,8 @@
+#Define a function which can compute the sum of two numbers.
+#Hints:
+#Define a function with two numbers as arguments. You can compute the sum in the function and return the value.
+def addnum(a,b):
+    return  a + b
+print(addnum(2,3))
+
+
