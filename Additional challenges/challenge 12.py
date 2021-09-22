0428732f92b323a46ea0d98643c0602644d97d19
Muhumuza James <65670517+Yakobo-UG@users.noreply.github.com>
2021-09-22 12:51:30 +0300

    challenge 12
    Please write a program which accepts basic mathematic expression from console and print the evaluation result.
    #Hints:
    #Use eval() to evaluate an expression.

---
+++ Additional challenges/challenge 12.py
@@ -0,0 +1,5 @@
+#Please write a program which accepts basic mathematic expression from console and print the evaluation result.
+#Hints:
+#Use eval() to evaluate an expression.
+UserInput = input("Enter values: ")
+print(eval(UserInput))
