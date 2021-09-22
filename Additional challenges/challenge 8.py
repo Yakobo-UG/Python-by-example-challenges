28e0214090e3ae64d0b6379485c80fcd6a80550d
Muhumuza James <65670517+Yakobo-UG@users.noreply.github.com>
2021-09-22 12:50:26 +0300

    challenge 8
    Define a function that can receive two integral numbers in string form and compute their sum and then print it in console.
    #Hints:
    #Use int() to convert a string to integer.

---
+++ Additional challenges/challenge 8.py
@@ -0,0 +1,7 @@
+#Define a function that can receive two integral numbers in string form and compute their sum and then print it in console.
+#Hints:
+#Use int() to convert a string to integer.
+def TwoInt(a,b):
+    return int(a + b)
+print(TwoInt(2,3))
+print(type(TwoInt(2,3)))
