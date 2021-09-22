e8a275a108232af4891b0e1321ecdb78d288282c
Muhumuza James <65670517+Yakobo-UG@users.noreply.github.com>
2021-09-22 12:50:40 +0300

    challenge 9
    Define a function that can accept two strings as input and concatenate them and then print it in console.
    #Hints:
    #Use + to concatenate the strings

---
+++ Additional challenges/challenge 9.py
@@ -0,0 +1,6 @@
+#Define a function that can accept two strings as input and concatenate them and then print it in console.
+#Hints:
+#Use + to concatenate the strings
+def sting(a,b):
+    print (str(a + b))
+print(sting("jamse","kamulin"))
