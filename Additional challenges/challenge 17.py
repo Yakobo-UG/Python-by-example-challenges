bbae8825d0675b1e31694367b773a08c5e5acf35
Muhumuza James <65670517+Yakobo-UG@users.noreply.github.com>
2021-09-29 16:24:47 +0300

    challenge 17
    Write a function to check if a list contains a particular number.

---
+++ Additional challenges/challenge 17.py
@@ -0,0 +1,9 @@
+#Write a function to check if a list contains a particular number.
+list = [1,2,3,4,5]
+def find(list):
+    for i in str(list):
+        if i == str(list):
+            return "Found the number"
+        else:
+            return "Not in the list"
+print(find(30))
