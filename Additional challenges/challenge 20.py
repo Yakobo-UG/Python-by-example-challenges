6b8719cf1b8f8912d8cc3624ee0c5b6377900e69
Muhumuza James <65670517+Yakobo-UG@users.noreply.github.com>
2021-09-29 16:25:38 +0300

    challenge 20
    Create a function that returns True if a string is empty and False otherwise.

---
+++ Additional challenges/challenge 20.py
@@ -0,0 +1,7 @@
+#Create a function that returns True if a string is empty and False otherwise.
+def string(word):
+    if word == " ":
+        return True
+    else:
+        return False
+print(string(" "))
