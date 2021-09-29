378df6022e4f7bfd6722c935556f53f27269a242
Muhumuza James <65670517+Yakobo-UG@users.noreply.github.com>
2021-09-29 16:26:06 +0300

    challenge 21
    Create a function that accepts a list and returns the last item in the list. The list can be either homogeneous or heterogeneous.

---
+++ Additional challenges/challenge 21.py
@@ -0,0 +1,5 @@
+#Create a function that accepts a list and returns the last item in the list. The list can be either homogeneous or heterogeneous.
+def list(items):
+    return items[-1]
+list1 = ["Jimmy", "Kate", "Molly", "Zick"]
+print(list(list1))
