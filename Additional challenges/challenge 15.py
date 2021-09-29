b2ae8b7c978bc88e880971979d829f3ffc7ebdbd
Muhumuza James <65670517+Yakobo-UG@users.noreply.github.com>
2021-09-29 16:24:07 +0300

    challenge 15
    Given a list of integers, return the difference between the largest and smallest integers in the list.

---
+++ Additional challenges/challenge 15.py
@@ -0,0 +1,8 @@
+#Given a list of integers, return the difference between the largest and smallest integers in the list.
+list = [1,2,3,4,5]
+def diference(list):
+    return max(list) - min(list)
+
+print(diference(list))
+
+
