8451ca637286b46314dcad5beee25607acca2170
Muhumuza James <65670517+Yakobo-UG@users.noreply.github.com>
2021-09-30 18:18:17 +0300

    challenge 27
    Write a program that works out whether if a given number is an odd or even number.

---
+++ Additional challenges/challenge 27.py
@@ -0,0 +1,6 @@
+#Write a program that works out whether if a given number is an odd or even number.
+num = int(input("Enter number: "))
+if num%2 == 0:
+    print("The number is even")
+else:
+    print("The number is odd")
