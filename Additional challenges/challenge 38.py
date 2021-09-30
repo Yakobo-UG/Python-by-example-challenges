8a321d5a7c4b3b897c616ef63858ffbb4096d997
Muhumuza James <65670517+Yakobo-UG@users.noreply.github.com>
2021-09-30 18:22:53 +0300

    challenge 38
    Create a function that returns True if an integer is divisible by 5, and false otherwise.

---
+++ Additional challenges/challenge 38.py
@@ -0,0 +1,7 @@
+#Create a function that returns True if an integer is divisible by 5, and false otherwise.
+def num(num1):
+    if num1%5 == 0:
+        return True
+    else:
+        False
+print(num(3000))
